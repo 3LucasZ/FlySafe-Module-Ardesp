cef8000f217dda2587ed2c26cf9238999b9e3e30
3LucasZ <lucas.zheng@warriorlife.net>
2023-11-22 01:59:29 -0800

    Update LIDARLite.h

--- src/LIDARLite.h
+++ src/LIDARLite.h
@@ -6,11 +6,6 @@
 
 #include <Arduino.h>
 
-// void lidar_begin(int = 0, bool = false, char = LIDARLITE_ADDR_DEFAULT);
-// void lidar_configure(int = 0, char = LIDARLITE_ADDR_DEFAULT);
-// void lidar_setI2Caddr(char, char, char = LIDARLITE_ADDR_DEFAULT);
-// void lidar_reset(char = LIDARLITE_ADDR_DEFAULT);
-
 #ifdef __cplusplus
 extern "C" 
 {
@@ -23,9 +18,5 @@ int lidar_distance(bool biasCorrection, char lidarliteAddress);
 #ifdef __cplusplus
 }
 #endif
-
-// void lidar_write(char, char, char = LIDARLITE_ADDR_DEFAULT);
-// void lidar_read(char, int, byte*, bool, char);
-// void lidar_correlationRecordToSerial(char = '\n', int = 256, char = LIDARLITE_ADDR_DEFAULT);
 
 #endif
