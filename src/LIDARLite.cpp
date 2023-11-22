0f62a95241a92e0c515287d9f253c3ee67265e75
3LucasZ <lucas.zheng@warriorlife.net>
2023-11-22 01:56:08 -0800

    Update LIDARLite.cpp

--- src/LIDARLite.cpp
+++ src/LIDARLite.cpp
@@ -1,28 +1,3 @@
-/*------------------------------------------------------------------------------
-
-  LIDARLite Arduino Library
-  LIDARLite.cpp
-
-  This library provides quick access to the basic functions of LIDAR-Lite
-  via the Arduino interface. Additionally, it can provide a user of any
-  platform with a template for their own application code.
-
-  Copyright (c) 2016 Garmin Ltd. or its subsidiaries.
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
-------------------------------------------------------------------------------*/
-
 #define LEGACY_I2C
 
 #include <Arduino.h>
@@ -30,11 +5,6 @@
 #include <stdarg.h>
 #include "LIDARLite.h"
 
-/*------------------------------------------------------------------------------
-  Constructor
-
-  Use begin to initialize.
-------------------------------------------------------------------------------*/
 
 /*------------------------------------------------------------------------------
   Begin
