bd55b2e90f1d87718bfcfb58374fa0a04265a737
3LucasZ <lucas.zheng@warriorlife.net>
2023-12-22 16:53:10 -0800

    TRY: 3 concurrent connections

--- src/config.h
+++ src/config.h
@@ -1,9 +1,10 @@
 #ifndef APP_CONFIG_H
 #define APP_CONFIG_H
 
-#define AP_SSID "FlySafeAP"
+#define ID "1"
+#define AP_SSID "FlySafeAP-"+ID
 #define AP_PW "FlySafePW"
-#define MAX_STA_CONN 1
+#define MAX_STA_CONN 3
 #define BROADCAST_PERIOD 250
 
 #endif
